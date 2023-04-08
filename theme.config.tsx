dd769044acc64c99beb49f013e980a4bcc67a3b9
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-04-09 00:36:00 +0530

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/hrsh22/reward-system-docs',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/hrsh22/reward-system-docs',
+  footer: {
+    text: 'Reward System Docs',
+  },
+}
+
+export default config
