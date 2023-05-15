8ca2f0534c42e58c7684ecd0b88a3e4172176c12
RonikParashar <ronikparashar123@gmail.com>
2023-05-16 01:51:06 +0530

    haha

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>PerkVenue</span>,
   project: {
     link: 'https://github.com/hrsh22/reward-system-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+
   docsRepositoryBase: 'https://github.com/hrsh22/reward-system-docs',
   footer: {
     text: 'Reward System Docs',
